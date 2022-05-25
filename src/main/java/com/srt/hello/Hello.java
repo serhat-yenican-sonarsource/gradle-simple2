5f70c1cdd739ee8ea722359e6d783421a342a9f9
serhat-yenican-sonarsource <serhat.yenican@sonarsource.com>
2022-05-25 23:56:49 +0200

    all fixed

--- src/main/java/com/srt/hello/Hello.java
+++ src/main/java/com/srt/hello/Hello.java
@@ -1,33 +1,20 @@
 package com.srt.hello;
 
-import java.net.PasswordAuthentication;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.util.StringTokenizer;
+import java.util.logging.Logger;
 
 public class Hello {
 
-  public static String GREETING = "Hello world!";
+  public static final String GREETING = "com.srt.hello.Hello world!";
+
+  private static final Logger LOGGER = Logger.getLogger(Hello.class.getName());
 
   public static void main(String []args) {
     new Hello().hello(GREETING);
   }
 
   public void hello(String str) {
-    System.out.println(str);
+    LOGGER.info(str);
   }
 
-  public PasswordAuthentication hotSpotExample() throws SQLException {
-    Connection conn = null;
-    conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-            "user=steve&password=blue"); // Sensitive
-    String uname = "steve";
-    String password = "blue";
-    conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
-            "user=" + uname + "&password=" + password); // Sensitive
-
-    return new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
-  }
 }
 
