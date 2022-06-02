c201eddca0a26a0160d91e8530b07e89d981286a
serhat-yenican-sonarsource <serhat.yenican@sonarsource.com>
2022-06-02 20:47:47 +0200

    fixed

--- src/main/java/com/srt/hello/Hello.java
+++ src/main/java/com/srt/hello/Hello.java
@@ -1,36 +1,11 @@
 package com.srt.hello;
 
-
-import java.util.StringTokenizer;
-
 public class Hello {
   private static final String MESSAGE = "Hello";
-  private static final String APP_SECRET = "secret";
 
   public String hello(String name) {
     return MESSAGE + name;
   }
-
-  public static String getAppSecret() {
-    return APP_SECRET;
-  }
-
-  //TODO open this part later
-//  private void doSomething() {
-//    System.out.println(System.currentTimeMillis());
-//  }
-
-
-  public void verifyEmail(String email) throws Exception {
-    if(email == null) {
-      System.out.println("email is null");
-    }
-    if(!email.contains("@")) {
-      throw new Exception("invalid email");
-    }
-  }
-
-
 
 }
 
